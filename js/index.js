1c83d388300a34170fe4526c564c95fcaab5235c
orsolon <debora.orsolon@terra.com.br>
2019-10-07 17:27:39 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+document.querySelector('nav>button').addEventListener(
+
+//Evento a ser tratado(clicl)
+'click'
+
+//Função que executa qd o bt é clicado
+Function() {
+
+    //capturando a UL do menu
+    let ul= document.querySelector('nav>ul');
+
+    //Alterando a altira da ul;
+    ul.style.height = '400px'
+}
+
+
+
+)
+
